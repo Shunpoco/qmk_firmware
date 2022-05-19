b0fc851e491b8d6955e932516498277c09fef5bc
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-19 21:29:22 +0900

    Up to Raise

--- keyboards/omkbd/ergodash/rev1/keymaps/shunpoco/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/shunpoco/keymap.c
@@ -25,16 +25,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  | Lower|                    | Raise|   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  | Shift|                    | RCMD |   N  |   M  |   ,  |   .  |   /  |  Up  |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | Shift|                    | RCMD |   N  |   M  |   ,  |   .  |   /  | Raise|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl | LCMD |  ALt | Lower||||||||  ALt | LCMD | Space|||||||| Enter| Shift| Bksp |||||||| Raise| Down |  Up  | Right|
+   * | Ctrl | LCMD |  ALt | Lower||||||||  ALt | LCMD | Space|||||||| Enter| Shift| Bksp |||||||| Raise| Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                          KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TILD,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                          KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    LOWER  ,                          RAISE  , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,                          KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,                          KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RAISE,
     KC_LCTL, KC_LCMD, KC_LALT, LOWER,            KC_LALT, KC_LCMD, KC_SPC,         KC_ENT,  KC_RSFT, KC_BSPC,          RAISE,   KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
@@ -46,16 +46,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  | Lower|                    | Raise|   H  | Left | Down | Right|   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  | Shift|                    | RCMD |   N  |   M  |   ,  |   .  |   /  |  Up  |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | Shift|                    | RCMD |   N  |   M  |   ,  |   .  |   /  | Raise|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl | LCMD |  ALt | Lower||||||||  ALt | LCMD | Space|||||||| Enter| Shift| Bksp |||||||| Raise| Down |  Up  | Right|
+   * | Ctrl | LCMD |  ALt | Lower||||||||  ALt | LCMD | Space|||||||| Enter| Shift| Bksp |||||||| Raise| Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                          KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TILD,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                          KC_EQL , KC_Y,    KC_U,    KC_UP,   KC_O,    KC_P,    KC_BSLS,
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    LOWER  ,                          RAISE  , KC_H,    KC_LEFT, KC_DOWN, KC_RIGHT,KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,                          KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    LOWER  ,                          RAISE  , KC_H,    KC_LEFT, KC_DOWN, KC_RGHT,KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,                          KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RAISE,
     KC_LCTL, KC_LCMD, KC_LALT, LOWER,            KC_LALT, KC_LCMD, KC_SPC,         KC_ENT,  KC_RSFT, KC_BSPC,          RAISE,   KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
@@ -63,20 +63,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * | ESC  |   1  |   2  |   3  |   4  |   5  |   [  |                    |   ]  |   6  |   7  |   8  |   9  |   0  |Pscree|
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   [  |   ]  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * | Tab  |   Q  |   W  |   [  |   ]  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |  Up  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   {  |   }  |   G  | Lower|                    | Raise|   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | Ctrl |   A  |   S  |   {  |   }  |   G  | Lower|                    | Raise|   H  |   J  | Left | Down | Right|  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   (  |   )  |   B  | Shift|                    | RCMD |   N  |   M  |   ,  |   .  |   /  |  Up  |
+   * | Shift|   Z  |   X  |   (  |   )  |   B  | Shift|                    | RCMD |   N  |   M  |   ,  |   .  |   /  | Raise|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl | LCMD |  ALt | Lower||||||||  ALt | LCMD | Space|||||||| Enter| Shift| Bksp |||||||| Raise| Down |  Up  | Right|
+   * | Ctrl | LCMD |  ALt | Lower||||||||  ALt | LCMD | Space|||||||| Enter| Shift| Bksp |||||||| Raise| Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                          KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TILD,
-    KC_TAB,  KC_Q,    KC_W,    KC_LBRC, KC_RBRC, KC_SLSH, KC_MINS,                          KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_LCTL, KC_A,    KC_S,    KC_LCBR, KC_RCBR, KC_UNDS, LOWER  ,                          RAISE  , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_LPRN, KC_RPRN, KC_PLUS, KC_LSFT,                          KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
+    KC_TAB,  KC_Q,    KC_W,    KC_LBRC, KC_RBRC, KC_SLSH, KC_MINS,                          KC_EQL , KC_Y,    KC_U,    KC_I,    KC_UP,   KC_P,    KC_BSLS,
+    KC_LCTL, KC_A,    KC_S,    KC_LCBR, KC_RCBR, KC_UNDS, LOWER  ,                          RAISE  , KC_H,    KC_J,    KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_LPRN, KC_RPRN, KC_PLUS, KC_LSFT,                          KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT , KC_SLSH, RAISE,
     KC_LCTL, KC_LCMD, KC_LALT, LOWER,            KC_LALT, KC_LCMD, KC_SPC,         KC_ENT,  KC_RSFT, KC_BSPC,          RAISE,   KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
