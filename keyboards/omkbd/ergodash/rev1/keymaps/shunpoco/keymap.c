14820ace3f7fc123c33f79202aaefb3a60e72beb
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-20 19:00:39 +0900

    Fix

--- keyboards/omkbd/ergodash/rev1/keymaps/shunpoco/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/shunpoco/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * | ESC  |   1  |   2  |   3  |   4  |   5  |   [  |                    |   ]  |   6  |   7  |   8  |   9  |   0  |  `   |
+   * | ESC  |   F1 |   F2 |   F3 |   F4 |   F5 |  F11 |                    |  F12 |   F6 |   F7 |   F8 |   F9 | F10  |  `   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   -  |                    |   =  |   Y  |   U  |  UP  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                          KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11 ,                          KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_GRV,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                          KC_EQL , KC_Y,    KC_U,    KC_UP,   KC_O,    KC_P,    KC_BSLS,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    LOWER  ,                          RAISE  , KC_H,    KC_LEFT, KC_DOWN, KC_RGHT,KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,                          KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RAISE,
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Raise
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * | ESC  |   1  |   2  |   3  |   4  |   5  |   [  |                    |   ]  |   6  |   7  |   8  |   9  |   0  |  `   |
+   * | ESC  |   F1 |   F2 |   F3 |   F4 |   F5 |  F11 |                    |  F12 |   F6 |   F7 |   F8 |   F9 | F10  |  `   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   [  |   ]  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |  Up  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                          KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11 ,                          KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_GRV,
     KC_TAB,  KC_Q,    KC_W,    KC_LBRC, KC_RBRC, KC_SLSH, KC_MINS,                          KC_EQL , KC_Y,    KC_U,    KC_I,    KC_UP,   KC_P,    KC_BSLS,
     KC_LCTL, KC_A,    KC_S,    KC_LCBR, KC_RCBR, KC_UNDS, LOWER  ,                          RAISE  , KC_H,    KC_J,    KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_LPRN, KC_RPRN, KC_PLUS, KC_LSFT,                          KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT , KC_SLSH, RAISE,
